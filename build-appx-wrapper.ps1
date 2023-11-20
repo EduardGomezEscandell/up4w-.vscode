7831622e12a1221e6d6eba08c15039f782ff6c5b
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-11-20 10:08:07 +0100

    Simplified build for E2E

---
+++ build-appx-wrapper.ps1
@@ -0,0 +1,33 @@
+
+param (
+    [Parameter(Mandatory = $true, HelpMessage = "production, end_to_end_tests.")]
+    [string]$Mode,
+
+    [Parameter(Mandatory = $true, HelpMessage = "Path were to store the artifacts")]
+    [string]$InstallPath
+)
+
+if ( "${mode}" -eq 'end_to_end_tests' ) {
+    $env:UP4W_TEST_WITH_MS_STORE_MOCK = 1
+}
+
+powershell.exe -File .\tools\build\build-appx.ps1 -Mode ${mode}
+
+if ( "$?" -ne "True" ) {
+    Exit(1)
+}
+
+$artifacts = (
+    Get-ChildItem ".\msix\UbuntuProForWindows\AppPackages\UbuntuProForWindows_*"    `
+    | Sort-Object LastWriteTime                                                     `
+    | Select-Object -last 1                                                         `
+)
+
+$certficate = Get-Item "${artifacts}\*.cer"
+$package    = Get-Item "${artifacts}\*.msixbundle"
+
+Write-Output "Copying ${certficate} into ${InstallPath}"
+Copy-Item -Path "${certficate}" -Destination "${InstallPath}\certificate.cert"
+
+Write-Output "Copying ${package} into ${InstallPath}"
+Copy-Item -Path "${package}" -Destination "${InstallPath}\UbuntuProForWindows_${mode}.msixbundle"
